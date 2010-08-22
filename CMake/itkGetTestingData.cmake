9d0adc5ef24de456f83ff6afe9e46cebe225610e
Luis ibanez <luis.ibanez@kitware.com>
2010-08-22 15:20:02 -0400

    Initial configuration for MIDASapp data download
    Adding CMake macros and configuration to use the MIDASapp
    for downloading Testing/Data from MIDAS.

---
+++ CMake/itkGetTestingData.cmake
@@ -0,0 +1,27 @@
+find_program( MIDAScli_EXECUTABLE MIDAScli )                                  #find the MIDAscli exectutable
+
+macro( get_testing_data TEST_NAME DESTINATION_DIR PATH_TO_ITEM_OR_BITSTREAM ) #The macro needs 3 parameters: name of test,the directory where to pull the data and the path of the data in the server
+
+  if( DEFINED MCN )                                                           # MCN = Macro Call Number: to distinguish each call of the macro
+    math(EXPR MCN "${MCN}+1" )
+  else()
+    set( MCN 1 )
+  endif()
+
+
+  set( DIN 1 )                                                                # DIN = Downloaded Item Number: to distinguish the items that are dowloaded
+
+  foreach( test_download_data ${PATH_TO_ITEM_OR_BITSTREAM} )                  #The variable takes the value of the different items
+
+    add_test( ${TEST_NAME}_download_data_${MCN}_${DIN}
+      ${MIDAScli_EXECUTABLE} --database ${MIDASdatabase} pull -d ${DESTINATION_DIR}  #pulling the data into the destination directory
+       -p -r ${test_download_data} ${MIDASurl} )
+
+    set_property( TEST ${TEST_NAME} APPEND PROPERTY                           #The main test (TEST_NAME) depends on each download data test (TEST_NAME_downlad_data_MCN_DIN)
+      DEPENDS ${TEST_NAME}_download_data_${MCN}_${DIN} )
+
+    math( EXPR DIN "${DIN}+1" )                                               #Each time the macro is called, MCN is increased by 1
+
+  endforeach( test_download_data )
+
+endmacro()
